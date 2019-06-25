2a72e82150dd63ccef3ac21df8d86dfdb0c8c1f2
Akhand Mishra <akkimysite@gmail.com>
2019-06-25 19:56:17 +0530

    Updated location JSONs

--- api/free.py
+++ api/free.py
@@ -1,8 +1,18 @@
 import io
 import json
 
-fp = open('scraped.json', encoding="utf-8")
+fp = open('apartment_buy_locations.json', encoding="utf-8")
 data = json.load(fp)
 
-with io.open('scraped.json', 'w', encoding="utf-8") as fp:
-	json.dump(data, fp, ensure_ascii=False)
+newdata = {}
+
+for province in data.keys():
+	newdata[province] = {}
+	for city in data[province].keys():
+		if len(data[province][city].keys()) == 0:
+			continue
+		newdata[province][city] = data[province][city]
+
+
+with io.open('apartment_buy_locations.json', 'w', encoding="utf-8") as fp:
+	json.dump(newdata, fp, ensure_ascii=False)
