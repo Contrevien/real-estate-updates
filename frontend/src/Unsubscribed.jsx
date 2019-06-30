86648f59ca7a6e9bf14bed22e7ebd1c9d226e794
Akhand Mishra <akkimysite@gmail.com>
2019-06-30 17:07:40 +0530

    Frontend finalized

---
+++ frontend/src/Unsubscribed.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import './Unsubscribed.css'
+
+class Unsubscribed extends Component {
+    state = {
+        email: this.props.match.params.email,
+        message: 0
+    }
+
+    componentDidMount() {
+        fetch('/unsubscribe', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email: this.state.email,
+                key: "dontplease"
+            })
+        })
+            .then(res => res.json())
+            .then(res => {
+                if(res === "777") {
+                    this.setState({
+                        message: 2
+                    })
+                } else if(res === "666") {
+                    this.setState({
+                        message: 1
+                    })
+                }
+            })
+            .catch(err => {
+                this.setState({
+                    message: 2
+                })
+            })
+    }
+    
+    render() {
+
+        let message = this.state.message;
+        let toShow = null;
+
+        if(message === 0) {
+            toShow = <><h1>You have successfully Unsubscribed from the service.</h1>
+            <p>We hope to have helped you!</p></>
+        } else if(message === 1) {
+            toShow = <><h1>Hmm, Something is fishy</h1>
+            <p>Perhaps you visited the wrong link?</p></>
+        } else {
+            toShow = <h1>Some fault occured, please try again!</h1>
+        }
+
+        return (
+            <div className="Unsubscribed">
+                {toShow}
+            </div>
+        );
+    }
+}
+
+export default Unsubscribed;
---
+++ frontend/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './NotFound.css';
+
+class NotFound extends Component {
+    render() {
+        return (
+            <div className="NotFound">
+                <h1>Page not found.</h1>
+            </div>
+        );
+    }
+}
+
+export default NotFound;
